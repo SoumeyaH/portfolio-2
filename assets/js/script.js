77bc39961f50338eb2b701b360c31c619dd296dc
Soumeya <soumeyahassan47@gmail.com>
2021-04-30 22:02:05 +0100

    wip portfolio card is ugly

--- assets/js/script.js
+++ assets/js/script.js
@@ -161,84 +161,81 @@ const renderAboutMeContent = () => {
 
   const aboutMeContent = `
   <div class="container text-center" id="about-container">
-          <div class="row my-4">
-            <h1 class="display-1">About Me</h1>
-          </div>
-          <div class="row my-4">
-            <div class="col-sm">
-              <p class="font-monospace fs-5">
-                My name is Soumeya Hassan. I live in Birmingham UK with my
-                family. I recently started to learn to code. You can check out
-                some off my cool work in my portfolio.
-              </p>
-            </div>
-            <div class="row my-4">
-              <h2>Languages and Frameworks</h2>
-            </div>
-            <div class="row tech-icons-container">
-              <div class="col-sm tech-icon-container">
-                <img
-                  src="./assets/images/technologies/html5.png"
-                  alt=""
-                  class="tech-logo-image"
-                  title="HTML 5"
-                />
-              </div>
-              <div class="col-sm">
-                <img
-                  src="./assets/images/technologies/css3.png"
-                  alt=""
-                  class="tech-logo-image"
-                  title="CSS3"
-                />
-              </div>
-              <div class="col-sm">
-                <img
-                  src="./assets/images/technologies/javascript.png"
-                  alt=""
-                  class="tech-logo-image"
-                  title="JavaScript"
-                />
-              </div>
-            </div>
-            <div class="row">
-              <div class="col-sm">
-                <img
-                  src="./assets/images/technologies/bootstrap.png"
-                  alt=""
-                  class="tech-logo-image"
-                  title="Bootstrap"
-                />
-              </div>
-              <div class="col-sm">
-                <img
-                  src="./assets/images/technologies/jQuery.png"
-                  alt=""
-                  class="tech-logo-image"
-                  title="jQuery"
-                />
-              </div>
-              <div class="col-sm">
-                <img
-                  src="./assets/images/technologies/nodeJS.png"
-                  alt=""
-                  class="tech-logo-image"
-                  title="Node.js"
-                />
-              </div>
-              <div class="row">
-                <div class="col-sm">
-                  <img
-                    src="./assets/images/social/github-icon.png"
-                    alt=""
-                    class="tech-logo-image"
-                    title="GitHub"
-                  />
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>`;
+  <div class="row my-4">
+    <h1 class="display-1">About Me</h1>
+  </div>
+  <div class="row my-4">
+    <div class="col-sm font-monospace fs-5">
+      <p>Hi, my name is Soumeya Hassan.</p>
+      <p>I live in Birmingham UK with my family.</p>
+      <p>
+        I recently started to learn to code. You can check out some off my
+        cool work in my portfolio.
+      </p>
+    </div>
+    <div class="row my-4">
+      <h2>Languages and Frameworks</h2>
+    </div>
+    <div class="tech-icons-container">
+      <div>
+        <img
+          src="./assets/images/technologies/html5.png"
+          alt=""
+          class="tech-logo-image"
+          title="HTML 5"
+        />
+      </div>
+      <div>
+        <img
+          src="./assets/images/technologies/css3.png"
+          alt=""
+          class="tech-logo-image"
+          title="CSS3"
+        />
+      </div>
+      <div>
+        <img
+          src="./assets/images/technologies/javascript.png"
+          alt=""
+          class="tech-logo-image"
+          title="JavaScript"
+        />
+      </div>
+      <div>
+        <img
+          src="./assets/images/technologies/bootstrap.png"
+          alt=""
+          class="tech-logo-image"
+          title="Bootstrap"
+        />
+      </div>
+      <div>
+        <img
+          src="./assets/images/technologies/jQuery.png"
+          alt=""
+          class="tech-logo-image"
+          title="jQuery"
+        />
+      </div>
+      <div>
+        <img
+          src="./assets/images/technologies/nodeJS.png"
+          alt=""
+          class="tech-logo-image"
+          title="Node.js"
+        />
+      </div>
+      <div>
+        <img
+          src="./assets/images/social/github-icon.png"
+          alt=""
+          class="tech-logo-image"
+          title="GitHub"
+        />
+      </div>
+    </div>
+  </div>
+</div> `;
 
   $("#main-container").append(aboutMeContent);
 };
